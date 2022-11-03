bb2e7f83733ec55fe23657a3520fd265ff7bff57
papinianus <dglmsksooah@gmail.com>
2022-11-03 22:36:56 +0900

    task 213

---
+++ courses_3/tasks_213/669701.cs
@@ -0,0 +1,58 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using static template.UtilIo;
+
+namespace template
+{
+
+    public static class Program
+    {
+        public static void Main(string[] args) => Solver.Solve();
+    }
+
+    public static class Solver
+    {
+        public static void Solve()
+        {
+            GetInt();
+            GetInts().Max().Echo();
+        }
+    }
+
+    public static class UtilIo
+    {
+        private static string Read() => Console.ReadLine();
+        private const char Separator = ' ';
+        public static void Echo(this string val) => Console.WriteLine(val);
+        public static void Echo(this int val) => Console.WriteLine(val);
+        public static void Echo(this char val) => Console.WriteLine(val);
+        private static void Tell(this bool val, string truly = "Yes", string falsy = "No")
+        {
+            if (val)
+            {
+                Console.WriteLine(truly);
+                return;
+            }
+            Console.WriteLine(falsy);
+        }
+
+        public static void YesNo(this bool val) => val.Tell("Yes", "No");
+
+        public static string GetString() => Read();
+        public static string[] GetStringMultiple(int rowCount)
+        {
+            var results = new List<string>();
+            while (rowCount > 0)
+            {
+                results.Add(GetString());
+                rowCount--;
+            }
+            return results.ToArray();
+        }
+        public static int GetInt() => int.Parse(Read());
+        public static int[] GetInts() => Read().Split(Separator).Select(int.Parse).ToArray();
+        public static long GetLong() => long.Parse(Read());
+        public static long[] GetLongs() => Read().Split(Separator).Select(long.Parse).ToArray();
+    }
+}
